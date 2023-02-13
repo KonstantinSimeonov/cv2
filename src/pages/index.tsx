eda54432c23206982799e280ccfb9008cdfc6f50
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-02-13 18:35:04 +0200

    you can look at my fancy mug now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,8 +19,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <header>
+        <header className={`${styles.stack}`}>
           <h1 {...int}>Konstantin Simeonov</h1>
+          <div
+            className={`${styles.cluster} ${styles.between} ${styles.padding}`}
+          >
+            {data.personalInfo.contacts.map((c) => (
+              <Link
+                key={c.url}
+                className={`${styles.card} ${int.className}`}
+                href={c.url}
+              >
+                <strong>{c.text} <span>-&gt;</span></strong>
+              </Link>
+            ))}
+            <Image
+              className={styles.photo}
+              src="/konche-bonche.jpg"
+              height="150"
+              width="150"
+              alt="pic of me"
+            />
+          </div>
         </header>
         <article>
           <h2 {...int}>Skills</h2>
@@ -50,27 +70,29 @@ export default function Home() {
           <h2 {...int}>Work Experience</h2>
           <div className={styles.split}>
             {(() => {
-              const leakySod = <div
-                      style={{
-                        minHeight: `3rem`,
-                        flex: 1,
-                        alignSelf: `center`,
-                        justifySelf: `flex-end`,
-                        width: `2px`,
-                        background: `#ffb86c`,
-                        height: `3rem`,
-                      }}
-                    />
+              const leakySod = (
+                <div
+                  style={{
+                    minHeight: `3rem`,
+                    flex: 1,
+                    alignSelf: `center`,
+                    justifySelf: `flex-end`,
+                    width: `2px`,
+                    background: `#ffb86c`,
+                    height: `3rem`,
+                  }}
+                />
+              );
               const we = data.workExperience.map((w, i) => (
                 <div
                   key={w.location}
                   className={`${styles.work} ${styles.stack}`}
                 >
-                  {i % 2 === 1 ? (
-                    leakySod
-                  ) : null}
+                  {i % 2 === 1 ? leakySod : null}
                   <div>
-                    <h4 {...int}>{w.location} ({w.jobTitle})</h4>
+                    <h4 {...int}>
+                      {w.location} ({w.jobTitle})
+                    </h4>
                     <time {...int}>
                       {w.from} - {w.to}
                     </time>{" "}
@@ -80,9 +102,7 @@ export default function Home() {
                       <p key={d}>{d}</p>
                     ))}
                   </ul>
-                  {i % 2 === 0 ? (
-                    leakySod
-                  ) : null}
+                  {i % 2 === 0 ? leakySod : null}
                 </div>
               ));
 
@@ -116,12 +136,17 @@ export default function Home() {
           <h2 {...int}>Projects</h2>
           <div className={styles.grid}>
             {data.projects.map((p) => (
-                <Link key={p.title} target="_blank" className={styles.card} href={p.links[0].url}>
-                  <h4 {...int}>
-                    {p.title} <span>-&gt;</span>
-                  </h4>
-                  <p {...int}>{p.description}</p>
-                </Link>
+              <Link
+                key={p.title}
+                target="_blank"
+                className={styles.card}
+                href={p.links[0].url}
+              >
+                <h4 {...int}>
+                  {p.title} <span>-&gt;</span>
+                </h4>
+                <p {...int}>{p.description}</p>
+              </Link>
             ))}
           </div>
         </article>
