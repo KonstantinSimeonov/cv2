b65ee955892be26990bdc9197ba9ae4c9dafaebc
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-03-01 17:56:50 +0200

    refactor: extract personal info component

---
+++ src/components/PersonalInfo.tsx
@@ -0,0 +1,31 @@
+import ExportedImage from "next-image-export-optimizer";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import ImgMap from "@/images";
+import * as React from "react";
+import { Stack } from "@/components/Stack";
+import { Text } from "@/components/Text";
+
+export const PersonalInfo: React.FC<{
+  contacts: readonly {
+    url: string;
+    text: string;
+  }[];
+}> = ({ contacts }) => (
+  <Stack direction="row" justify="space-between" sx={{ flexWrap: `nowrap` }}>
+    <Stack align="flex-start" gap={0.5}>
+      {contacts.map(({ url, text }) => (
+        <Link key={url} target="_blank" href={url}>
+          <Text>{text}</Text>
+        </Link>
+      ))}
+    </Stack>
+    <ExportedImage
+      className={styles.photo}
+      src={ImgMap.Photo}
+      height="150"
+      width="150"
+      alt="pic of me"
+    />
+  </Stack>
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
 import Head from "next/head";
-import ExportedImage from "next-image-export-optimizer";
 import styles from "@/styles/Home.module.css";
 import data from "@/data.json";
-import Link from "next/link";
-import ImgMap from "@/images";
-import * as React from "react";
 import { Skills } from "@/components/Skills";
 import { Stack } from "@/components/Stack";
 import { WorkExperience } from "@/components/WorkExperience";
 import { Text } from "@/components/Text";
 import { Footer } from "@/components/Footer";
-import {Projects} from "@/components/Projects";
+import { Projects } from "@/components/Projects";
+import { PersonalInfo } from "@/components/PersonalInfo";
 
 export default function Home() {
   return (
@@ -25,27 +22,7 @@ export default function Home() {
       <Stack tag="main" gap={2} className={styles.main}>
         <Stack tag="header" gap={1}>
           <Text tag="h1">Konstantin Simeonov</Text>
-
-          <Stack
-            direction="row"
-            justify="space-between"
-            sx={{ flexWrap: `nowrap` }}
-          >
-            <Stack align="flex-start" gap={0.5}>
-              {data.personalInfo.contacts.map((c) => (
-                <Link key={c.url} target="_blank" href={c.url}>
-                  <Text>{c.text}</Text>
-                </Link>
-              ))}
-            </Stack>
-            <ExportedImage
-              className={styles.photo}
-              src={ImgMap.Photo}
-              height="150"
-              width="150"
-              alt="pic of me"
-            />
-          </Stack>
+          <PersonalInfo contacts={data.personalInfo.contacts} />
         </Stack>
         <Stack tag="article" gap={1}>
           <Text tag="h2">Skills</Text>
