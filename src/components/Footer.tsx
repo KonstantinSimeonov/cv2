8054979fb01634afe475b5a3a3af932d080cb6f8
Konstantin Simeonov <kon.simeonov@protonmail.com>
2026-04-07 16:54:22 +0300

    improve(pdf): layout fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,18 @@
 import { Stack } from "@/components/Stack"
 import Link from "next/link"
 import classes from "./footer.module.css"
+import {data} from "@/data"
 
-export const Footer: React.FC<{ className?: string }> = ({ className }) => (
+export const Footer: React.FC = () => (
   <Stack
     tag="footer"
     direction="row"
     justify="space-evenly"
-    className={`${classes.footer} ${className}`}
+    className={classes.footer}
   >
     <a
       href="#"
+      className={classes.printLink}
       onClick={e => {
         e.preventDefault()
         window.print()
@@ -20,6 +22,9 @@ export const Footer: React.FC<{ className?: string }> = ({ className }) => (
     </a>
     <Link target="_blank" href="https://github.com/KonstantinSimeonov/cv2">
       Built with NextJS/AWS/Pulumi
+    </Link>
+    <Link className={classes.siteLink} target="_blank" href={data.site}>
+      Hosted at konsimeonov.com
     </Link>
   </Stack>
 )
